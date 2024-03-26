9d9b775f70b4ba64ba5183476ca0d6a94f284e8d
十字圣球 <52451163+Awesomemy@users.noreply.github.com>
2024-03-26 08:34:46 +0800

    Update and rename SumOfFour.java to AdditionOfFour.java

--- AdditionOfFour.java
+++ AdditionOfFour.java
@@ -3,6 +3,11 @@ package test4;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+* 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+
+0 <= i, j, k, l < n
+nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0*/
 public class SumOfFour {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         /*
